5ad5795657c85d3ff4ab51a0516e10605672bb25
xavvvier <xavvvier@users.noreply.github.com>
2020-02-01 14:00:16 -0400

    Remove unused fonts

--- lib/portfolio.ex
+++ lib/portfolio.ex
@@ -6,7 +6,4 @@ defmodule Portfolio do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
-  def hi(sata, data, lata) do
-    sata 
-  end
 end
