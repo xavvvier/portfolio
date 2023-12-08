9745818aeee786a4481de755305a25263df47673
xavvvier <xavvvier@users.noreply.github.com>
2023-12-08 10:16:28 -0400

    Update to phoenix 1.7

--- mix.exs
+++ mix.exs
@@ -7,7 +7,6 @@ defmodule Portfolio.MixProject do
       version: "0.1.2",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: Mix.compilers(),
       aliases: aliases(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -33,8 +32,9 @@ defmodule Portfolio.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.0"},
+      {:phoenix, "~> 1.7.0"},
       {:phoenix_html, "~> 3.0"},
+      {:phoenix_view, "~> 2.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.1"},
       {:floki, ">= 0.30.0", only: :test},
